df9792e9780c5a43752b577d2cbdeb3ffe7c3d55
Eugeny Rusakov <mornst@yandex.ru>
2019-02-03 10:45:51 +0300

    2 pane with scrolling

---
+++ SignalProcessing/SignalProcessing.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SignalProcessing
+{
+    partial class SignalProcessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hScrollBar = new System.Windows.Forms.HScrollBar();
+            this.SuspendLayout();
+            // 
+            // hScrollBar
+            // 
+            this.hScrollBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar.Location = new System.Drawing.Point(0, 429);
+            this.hScrollBar.Maximum = 40000;
+            this.hScrollBar.Name = "hScrollBar";
+            this.hScrollBar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.hScrollBar.Size = new System.Drawing.Size(800, 21);
+            this.hScrollBar.TabIndex = 0;
+            this.hScrollBar.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // SignalProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hScrollBar);
+            this.Name = "SignalProcessing";
+            this.Text = "SignalProcessing";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hScrollBar;
+    }
+}
+
---
+++ SignalProcessing/SignalProcessing.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalProcessing
+{
+    public partial class SignalProcessing : Form
+    {
+        public SignalProcessing()
+        {
+            InitializeComponent();
+
+            ResizeRedraw = true;
+
+            signal = new int[44100];
+
+            for (int i = 0; i < signal.Length; ++i)
+                signal[i] = (int)(Math.Sin(Math.PI * 2 * i * 1000 / 44100) * 100);
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Rectangle[] rectangles = new Rectangle[2];
+
+            for (int i = 0; i < rectangles.Length; ++i)
+            {
+                rectangles[i].X = ClientRectangle.X + 10;
+                rectangles[i].Y = ClientRectangle.Y + (ClientRectangle.Height - hScrollBar.Size.Height) * i / rectangles.Length + 10;
+                rectangles[i].Width = ClientRectangle.Width - 20;
+                rectangles[i].Height = (ClientRectangle.Height - hScrollBar.Size.Height) / rectangles.Length - 20;
+            }
+
+            Graphics g = CreateGraphics();
+
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            for (int i = 0; i < rectangles.Length; ++i)
+            {
+                g.DrawLine(blackPen, new Point(rectangles[i].X, rectangles[i].Y), new Point(rectangles[i].X, rectangles[i].Bottom));
+                g.DrawLine(blackPen, new Point(rectangles[i].X, rectangles[i].Y + rectangles[i].Height / 2), new Point(rectangles[i].Right, rectangles[i].Y + rectangles[i].Height / 2));
+            }
+
+
+            Point from = new Point(rectangles[0].X, rectangles[0].Y + rectangles[0].Height / 2);
+            from.Offset(0, -(signal[hScrollBar.Value] * rectangles[0].Height / 2 / 100));
+            Point to = new Point(0, 0);
+            for (int i = 1; i < rectangles[0].Width; ++i)
+            { 
+                to.X = i;
+                to.Y = - (signal[hScrollBar.Value + i] * rectangles[0].Height / 2 / 100);
+                to.Offset(rectangles[0].X, rectangles[0].Y + rectangles[0].Height / 2);
+                g.DrawLine(blackPen, from, to);
+                from.X = to.X;
+                from.Y = to.Y;
+            }
+
+            g.Dispose();
+                
+        }
+
+        private int [] signal;
+
+        private void OnValueChanged(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
