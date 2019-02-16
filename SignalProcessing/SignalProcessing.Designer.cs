1c565517828353f1a87ebd564c18bf2d28739e25
Eugeny Rusakov <mornst@yandex.ru>
2019-02-16 15:23:14 +0300

    Toolbox added for text boxes

--- SignalProcessing/SignalProcessing.Designer.cs
+++ SignalProcessing/SignalProcessing.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.hScrollBar = new System.Windows.Forms.HScrollBar();
-            this.frequency_1 = new System.Windows.Forms.TextBox();
-            this.frequency_2 = new System.Windows.Forms.TextBox();
-            this.zeroes = new System.Windows.Forms.TextBox();
-            this.timeFFT = new System.Windows.Forms.TextBox();
-            this.timeFilter = new System.Windows.Forms.TextBox();
-            this.windowLength = new System.Windows.Forms.TextBox();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.frequency_1 = new System.Windows.Forms.ToolStripTextBox();
+            this.frequency_2 = new System.Windows.Forms.ToolStripTextBox();
+            this.windowLength = new System.Windows.Forms.ToolStripTextBox();
+            this.zeroes = new System.Windows.Forms.ToolStripTextBox();
+            this.timeFFT = new System.Windows.Forms.ToolStripLabel();
+            this.timeFilter = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // hScrollBar
@@ -44,76 +46,77 @@
             this.hScrollBar.Maximum = 40000;
             this.hScrollBar.Name = "hScrollBar";
             this.hScrollBar.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.hScrollBar.Size = new System.Drawing.Size(600, 21);
-            this.hScrollBar.TabIndex = 0;
+            this.hScrollBar.Size = new System.Drawing.Size(729, 21);
+            this.hScrollBar.TabIndex = 6;
             this.hScrollBar.ValueChanged += new System.EventHandler(this.OnValueChanged);
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.frequency_1,
+            this.frequency_2,
+            this.windowLength,
+            this.zeroes,
+            this.timeFFT,
+            this.timeFilter});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(729, 25);
+            this.toolStrip.TabIndex = 7;
             // 
             // frequency_1
             // 
-            this.frequency_1.Location = new System.Drawing.Point(0, 1);
             this.frequency_1.Name = "frequency_1";
-            this.frequency_1.Size = new System.Drawing.Size(47, 20);
-            this.frequency_1.TabIndex = 1;
+            this.frequency_1.Size = new System.Drawing.Size(50, 25);
             this.frequency_1.Text = "1000";
             this.frequency_1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnFreq1);
             // 
             // frequency_2
             // 
-            this.frequency_2.Location = new System.Drawing.Point(53, 1);
             this.frequency_2.Name = "frequency_2";
-            this.frequency_2.Size = new System.Drawing.Size(45, 20);
-            this.frequency_2.TabIndex = 2;
+            this.frequency_2.Size = new System.Drawing.Size(50, 25);
             this.frequency_2.Text = "5000";
             this.frequency_2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnFreq2);
             // 
+            // windowLength
+            // 
+            this.windowLength.Name = "windowLength";
+            this.windowLength.Size = new System.Drawing.Size(50, 25);
+            this.windowLength.Text = "512";
+            this.windowLength.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnWindowLength);
+            // 
             // zeroes
             // 
-            this.zeroes.Location = new System.Drawing.Point(155, 1);
             this.zeroes.Name = "zeroes";
-            this.zeroes.Size = new System.Drawing.Size(29, 20);
-            this.zeroes.TabIndex = 3;
+            this.zeroes.Size = new System.Drawing.Size(25, 25);
             this.zeroes.Text = "2";
             this.zeroes.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnZeroes);
             // 
             // timeFFT
             // 
-            this.timeFFT.Location = new System.Drawing.Point(190, 1);
             this.timeFFT.Name = "timeFFT";
-            this.timeFFT.Size = new System.Drawing.Size(65, 20);
-            this.timeFFT.TabIndex = 4;
+            this.timeFFT.Size = new System.Drawing.Size(13, 22);
+            this.timeFFT.Text = "0";
             // 
             // timeFilter
             // 
-            this.timeFilter.Location = new System.Drawing.Point(261, 1);
             this.timeFilter.Name = "timeFilter";
-            this.timeFilter.Size = new System.Drawing.Size(65, 20);
-            this.timeFilter.TabIndex = 4;
-            // 
-            // windowLength
-            // 
-            this.windowLength.Location = new System.Drawing.Point(104, 1);
-            this.windowLength.Name = "windowLength";
-            this.windowLength.Size = new System.Drawing.Size(45, 20);
-            this.windowLength.TabIndex = 5;
-            this.windowLength.Text = "512";
-            this.windowLength.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnWindowLength);
+            this.timeFilter.Size = new System.Drawing.Size(13, 22);
+            this.timeFilter.Text = "0";
             // 
             // SignalProcessing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.windowLength);
-            this.Controls.Add(this.timeFilter);
-            this.Controls.Add(this.timeFFT);
-            this.Controls.Add(this.zeroes);
-            this.Controls.Add(this.frequency_2);
-            this.Controls.Add(this.frequency_1);
+            this.ClientSize = new System.Drawing.Size(729, 366);
+            this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.hScrollBar);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SignalProcessing";
             this.Text = "SignalProcessing";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,12 +125,13 @@
         #endregion
 
         private System.Windows.Forms.HScrollBar hScrollBar;
-        private System.Windows.Forms.TextBox frequency_1;
-        private System.Windows.Forms.TextBox frequency_2;
-        private System.Windows.Forms.TextBox zeroes;
-        private System.Windows.Forms.TextBox timeFFT;
-        private System.Windows.Forms.TextBox timeFilter;
-        private System.Windows.Forms.TextBox windowLength;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripTextBox frequency_1;
+        private System.Windows.Forms.ToolStripTextBox frequency_2;
+        private System.Windows.Forms.ToolStripTextBox windowLength;
+        private System.Windows.Forms.ToolStripTextBox zeroes;
+        private System.Windows.Forms.ToolStripLabel timeFFT;
+        private System.Windows.Forms.ToolStripLabel timeFilter;
     }
 }
 
